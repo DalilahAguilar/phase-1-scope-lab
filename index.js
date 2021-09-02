d94dd0c27f0018d90a897f7069385a07a6401235
Dalilah Aguilar <dalilahaguilar9@gmail.com>
2021-09-02 11:30:12 -0500

    commit tree 3cd3bb39d0c5fa15a49f9c81254e79cfb751b5dc

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob'
+let bestCustomer = 'not bob';
+const leastFavoriteCustomer = "bob"
+
+function upperCaseCustomerName(){
+console.log(customerName.toUpperCase())
+return upperCaseCustomerName()
+}
+function setBestCustomer(){
+    return bestCustomer;
+    }
+    overWriteBeestCustomer(){
+        let newBestCustomer = " maybe bob"
+    }
+    function changeLeastFavoriteCustomer(){
+        return changeLeastFavoriteCustomer(leastFavoriteCustomer);
+    }
